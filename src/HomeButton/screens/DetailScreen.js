8e61fa9974987ddddfdbc5776f7d6661f3b4c63e
[capelito-tv-show] <capelito.tv.show@gmail.com>
2018-06-24 17:12:26 +0900

    add geocoder and async function to DetailScreen

--- src/HomeButton/screens/DetailScreen.js
+++ src/HomeButton/screens/DetailScreen.js
@@ -4,10 +4,12 @@ import {
   View,
   ScrollView, // 描画に直接関わるコンポーネント達
   Dimensions,
-  Platform // ←追記部分 // 描画に直接関わらないもの
+  Platform,
+  ActivityIndicator
 } from 'react-native'
 import { MapView } from 'expo'
 import { connect } from 'react-redux'
+import Geocoder from 'react-native-geocoding'
 import * as actions from '../../actions'
 
 const SCREEN_WIDTH = Dimensions.get('window').width
@@ -17,6 +19,7 @@ class DetailScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      isMapLoaded: false, //地図の読み込み未完了 GeoCoder
       initialRegion: {
         latitude: 35.709, // 東京都の緯度
         longitude: 139.732, // 東京都の経度
@@ -25,7 +28,30 @@ class DetailScreen extends React.Component {
       }
     }
   }
+
+  async componentWillMount() {
+    Geocoder.setApiKey('AIzaSyC1kDfRxJQP6QgGKpQZcopPwRLn8khcz8E')
+
+    let result = await Geocoder.getFromLocation(this.props.detailReview.country)
+
+    this.setState({
+      isMapLoaded: true,
+      initialRegion: {
+        latitude: result.results[0].geometry.location.lat,
+        longitude: result.results[0].geometry.location.lng, // 変換後の経度
+        latitudeDelta: MAP_ZOOM_RATE, // 値自体は変わっていないが書く必要あり
+        longitudeDelta: MAP_ZOOM_RATE * 2.25 // 値自体は変わっていないが書く必要あり
+      }
+    })
+  }
   render() {
+    if (this.state.isMapLoaded === false) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      )
+    }
     return (
       <View style={{ flex: 1 }}>
         <ScrollView>
