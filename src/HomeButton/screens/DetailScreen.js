9830ad5e81a7a8150970b76555e4e576f2c97031
[capelito-tv-show] <capelito.tv.show@gmail.com>
2018-06-24 16:54:06 +0900

    add mapview to detailscreen

--- src/HomeButton/screens/DetailScreen.js
+++ src/HomeButton/screens/DetailScreen.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { ScrollView, Text, View } from 'react-native'
+import {
+  Text,
+  View,
+  ScrollView, // 描画に直接関わるコンポーネント達
+  Dimensions,
+  Platform // ←追記部分 // 描画に直接関わらないもの
+} from 'react-native'
+import { MapView } from 'expo'
 import { connect } from 'react-redux'
-
 import * as actions from '../../actions'
 
+const SCREEN_WIDTH = Dimensions.get('window').width
+const MAP_ZOOM_RATE = 15.0
+
 class DetailScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initialRegion: {
+        latitude: 35.709, // 東京都の緯度
+        longitude: 139.732, // 東京都の経度
+        latitudeDelta: MAP_ZOOM_RATE, // 緯度方向のズーム度合い
+        longitudeDelta: MAP_ZOOM_RATE * 2.25 // 経度方向のズーム度合い(緯度方向の2.25倍)
+      }
+    }
+  }
   render() {
     return (
       <View style={{ flex: 1 }}>
@@ -18,6 +38,12 @@ class DetailScreen extends React.Component {
               {this.props.detailReview.dateTo}
             </Text>
           </View>
+          <MapView
+            style={{ height: SCREEN_WIDTH }}
+            scrollEnabled={false}
+            cacheEnabled={Platform.OS === 'android'}
+            initialRegion={this.state.initialRegion}
+          />
         </ScrollView>
       </View>
     )
