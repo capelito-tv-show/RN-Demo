3eec2a455fed7da5c83d3406ba6940af4cc915c3
[capelito-tv-show] <capelito.tv.show@gmail.com>
2018-06-24 17:44:08 +0900

    create modal to detail screen

--- src/HomeButton/screens/DetailScreen.js
+++ src/HomeButton/screens/DetailScreen.js
@@ -5,6 +5,9 @@ import {
   ScrollView, // 描画に直接関わるコンポーネント達
   Dimensions,
   Platform,
+  Image,
+  Modal,
+  TouchableOpacity,
   ActivityIndicator
 } from 'react-native'
 import { MapView } from 'expo'
@@ -25,7 +28,9 @@ class DetailScreen extends React.Component {
         longitude: 139.732, // 東京都の経度
         latitudeDelta: MAP_ZOOM_RATE, // 緯度方向のズーム度合い
         longitudeDelta: MAP_ZOOM_RATE * 2.25 // 経度方向のズーム度合い(緯度方向の2.25倍)
-      }
+      },
+      modalVisible: false,
+      modalImageURI: require('../../assets/image_placeholder.png')
     }
   }
 
@@ -44,6 +49,50 @@ class DetailScreen extends React.Component {
       }
     })
   }
+
+  renderImages() {
+    const imageArray = [
+      { isImage: false, uri: require('../../assets/image_placeholder.png') },
+      { isImage: false, uri: require('../../assets/image_placeholder.png') },
+      { isImage: false, uri: require('../../assets/image_placeholder.png') }
+    ]
+
+    // 添付されている画像の数だけ繰り返す(最大3回繰り返される)
+    for (let i = 0; i < this.props.detailReview.imageURIs.length; i++) {
+      imageArray[i].isImage = true
+
+      // 添付画像の保存場所に更新
+      imageArray[i].uri = this.props.detailReview.imageURIs[i]
+    }
+
+    return (
+      // 縦ではなく横方向に3つ並べる
+      <View style={{ flexDirection: 'row' }}>
+        // `imageArray`の個数分(3つ)だけ繰り返す
+        {imageArray.map((image, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() =>
+                this.setState({
+                  modalVisible: image.isImage,
+                  modalImageURI: image.uri
+                })
+              }
+            >
+              <Image
+                // 縦も横も同じサイズ(スマホ画面の横幅÷3)、つまり正方形画像
+                style={{ height: SCREEN_WIDTH / 3, width: SCREEN_WIDTH / 3 }}
+                // 表示する画像のソース
+                source={image.uri}
+              />
+            </TouchableOpacity>
+          )
+        })}
+      </View>
+    )
+  }
+
   render() {
     if (this.state.isMapLoaded === false) {
       return (
@@ -54,6 +103,31 @@ class DetailScreen extends React.Component {
     }
     return (
       <View style={{ flex: 1 }}>
+        <Modal
+          visible={this.state.modalVisible}
+          animationType="fade"
+          transparent={false}
+        >
+          <View style={{ flex: 1, backgroundColor: 'black' }}>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              // タッチされたら、モーダルを非表示にする
+              onPress={() => this.setState({ modalVisible: false })}
+            >
+              <Image // ←追記ここから
+                // 縦も横も同じサイズ(スマホ画面の横幅)、つまり正方形画像
+                style={{ height: SCREEN_WIDTH, width: SCREEN_WIDTH }}
+                // 表示する画像のソース
+                source={this.state.modalImageURI}
+              />{' '}
+              // ←追記ここまで />
+            </TouchableOpacity>
+          </View>
+        </Modal>
         <ScrollView>
           <View style={{ alignItems: 'center', padding: 20 }}>
             <Text style={{ fontSize: 30, padding: 5 }}>
@@ -70,6 +144,7 @@ class DetailScreen extends React.Component {
             cacheEnabled={Platform.OS === 'android'}
             initialRegion={this.state.initialRegion}
           />
+          {this.renderImages()}
         </ScrollView>
       </View>
     )
